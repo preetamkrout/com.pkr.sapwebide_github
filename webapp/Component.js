da6501f7d100efce949b911c105a9fff5509ee83
preetamkajalrout <preetamkajalrout@gmail.com>
2017-02-27 06:32:58 +0000

    [FEATURE]Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.pkr.sapwebide_githubcom.pkr.sapwebid_github.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.pkr.sapwebide_githubcom.pkr.sapwebid_github.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
